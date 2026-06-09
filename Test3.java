60f8085054d3862a84ac6837c30d4eed40b1187f
池本 元希 <user02@telcation.com>
2026-06-09 16:30:40 +0900

    追加

--- Test3.java
+++ Test3.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Test3 {
     static void main() {
         int[] a = {1, 2, 3, 4, 5};
@@ -5,15 +7,17 @@ public class Test3 {
         b = a;  //aにもbにも同じ参照が入っている
         //a.clone()でコピぺできる
         IO.println(b[2]);
+        IO.println(Arrays.toString(b));
         b[2] = 9;
         IO.println(b[2]);
         IO.println(a[2]);
         IO.println(a);
         IO.println(b);
-        b = null;
+        //b = null;
         IO.println(b);
         IO.println(a.length);
         IO.println(b.length);
+        IO.println(Arrays.toString(b));
 
 
     }
